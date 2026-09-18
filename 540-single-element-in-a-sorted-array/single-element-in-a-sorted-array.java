f7ccfb525e9e9b77031d680187f512fba19b8827
Taruna Ezhumalai <taruna.ezhumalai@gmail.com>
2026-09-18 22:57:26 +0530

    Time: 0 ms (100.00%) | Memory: 52.8 MB (57.93%) - LeetSync

---
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int singleNonDuplicate(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            // Ensure mid is even
+            if (mid % 2 == 1) {
+                mid--;
+            }
+
+            // If the pair matches, the single element is on the right side
+            if (nums[mid] == nums[mid + 1]) {
+                left = mid + 2;
+            } else {
+                right = mid;
+            }
+        }
+
+        return nums[left];
+    }
+}
